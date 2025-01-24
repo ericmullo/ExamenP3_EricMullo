8b40241218bf9a0b86de998a5b3be8993a0f1032
laboratorio <laboratorio@UPOAULA10624>
2025-01-24 07:16:40 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP3_EricMullo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenP3_EricMullo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenP3_EricMullo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenP3_EricMullo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
