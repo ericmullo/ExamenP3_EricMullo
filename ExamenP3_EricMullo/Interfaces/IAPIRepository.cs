5a325c028cc5bb376701f63a4c2ebc9c958331fb
ericmullo <ericrodrigomullo@gmail.com>
2025-01-24 20:19:49 -0500

    Realize cambios en las carpetas

---
+++ ExamenP3_EricMullo/Interfaces/IAPIRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExamenP3_EricMullo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3_EricMullo.Interfaces
+{
+    internal interface IAPIRepository
+    {
+        public Task<AirportModel> GetResponseFromAPi(string name);
+    }
+}
